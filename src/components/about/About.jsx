1ea5c08dc297c164b7d49f8524a2175bb064028b
German <g44ravarotto@gmail.com>
2024-06-17 16:38:16 -0300

    works on About

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,16 +9,50 @@ const About = () => {
       >
         <DividerTag tag="About" />
         <div className="container mx-auto min-h-[82vh] bg-black/20 backdrop-blur-2xl rounded-xl flex items-center">
-          <div className="w-full min-h-[82vh]  grid-cols-1 grid-rows-8 grid sm:grid-cols-4 sm:grid-rows-4 gap-1">
-            <div className="w-full h-1/4 items-end justify-end  bg-slate-100 sm:col-span-2">1</div>
-            <div className="w-full h-3/4  bg-slate-300 row-span-3 sm:col-span-2 sm:row-span-3 sm:col-start-1 sm:row-start-2">
-              2
-            </div>
-            <div className="w-full h-1/4 bg-slate-500 sm:col-span-2 sm:col-start-3 sm:row-start-1">
-              3
-            </div>
-            <div className="w-full h-3/4 bg-slate-800 row-span-3 sm:col-span-2 sm:row-span-3 sm:col-start-3 sm:row-start-2">
-              4
+          <div className="w-full min-h-[82vh] flex justify-center items-center">
+            <div className="w-full min-h-[70vh] flex flex-col sm:flex-row ">
+              <div className="w-full sm:w-1/2 min-h-[35vh] sm:min-h-[70vh]  flex justify-center items-center">
+                <div className="w-full sm:w-[90%] min-h-[35vh] sm:min-h-[65vh]  flex flex-col justify-center items-center">
+                  <div className=" w-full min-h-[5vh] sm:min-h-[10vh] flex justify-center items-end ">
+                    <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-[44px] font-bold text-white font-primary text-center">
+                      Hello I'm German
+                    </h2>
+                  </div>
+                  <div className=" w-full min-h-[40vh] sm:min-h-[60vh] flex justify-center items-center ">
+                    <p className="text-xl sm:text-2xl md:text-3xl lg:text-[44px]  text-white font-secondary text-justify">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Repudiandae expedita animi veniam nam fugiat neque ea
+                      distinctio harum magni officia.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full sm:w-1/2 min-h-[35vh] sm:min-h-[70vh]  flex flex-col justify-center items-center">
+                <div className="w-full min-h-[5vh] sm:min-h-[5vh] flex justify-center items-end  ">
+                  <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-[44px] font-bold text-white font-primary text-center">
+                    Skills & Tools
+                  </h2>
+                </div>
+                <div className=" w-full min-h-[40vh] sm:min-h-[65vh] flex justify-center items-center  ">
+                  <div className=" w-[90%] min-h-[30vh] sm:min-h-[60vh] grid grid-cols-3 grid-rows-5 gap-4 ">
+                    <div className="bg-red-700 rounded-xl">1</div>
+                    <div>2</div>
+                    <div className="bg-red-700 rounded-xl">3</div>
+                    <div>4</div>
+                    <div className="bg-red-700 rounded-xl">5</div>
+                    <div>6</div>
+                    <div className="bg-red-700 rounded-xl">7</div>
+                    <div>8</div>
+                    <div className="bg-red-700 rounded-xl">9</div>
+                    <div>10</div>
+                    <div className="bg-red-700 rounded-xl">11</div>
+                    <div>12</div>
+                    <div className="bg-red-700 rounded-xl">13</div>
+                    <div>14</div>
+                    <div className="bg-red-700 rounded-xl">15</div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
