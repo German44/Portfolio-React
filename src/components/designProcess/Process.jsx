200d6d0d6b6ac0b5f76d0f6f05dcd391ff495743
German <g44ravarotto@gmail.com>
2024-08-05 22:21:25 -0300

    Update case study json

--- src/components/designProcess/Process.jsx
+++ src/components/designProcess/Process.jsx
@@ -1,10 +1,10 @@
 const Process = ({ title, content, image }) => {
   return (
-    <div className="w-full h-full px-8 mt-4 flex flex-col sm:grid sm:grid-cols-4 sm:grid-rows-6 gap-4 mt-8">
+    <div className="w-full h-full px-8 flex flex-col sm:grid sm:grid-cols-4 sm:grid-rows-8 gap-4 mt-8">
       <div className="sm:col-span-2 ">
         <h2 className="text-white font-primary text-xl sm:text-5xl m-4" id={title}>{title}</h2>
       </div>
-      <div className=" sm:col-span-2 sm:row-span-2 sm:col-start-3  ">
+      <div className=" sm:col-span-2 sm:row-span-1 sm:col-start-3  ">
         <p className="text-white  text-lg sm:text-xl text-justify m-4 mt-12">{content}</p>
       </div>
       <div className="sm:col-span-4 sm:row-span-4 sm:row-start-3 ">
--- src/components/designProcess/SphereColors.jsx
+++ src/components/designProcess/SphereColors.jsx
@@ -4,44 +4,44 @@ const SphereColors = () => {
   return (
     <div className="mt-4 px-8 w-full h-[80px] sm:h-[150px] flex justify-center items-center gap-1 sm:gap-4 ">
       <LinkScroll
-        to="Define"
+        to="Empatizar"
         smooth={true}
         spy={true}
         className="font-bold text-[10px] sm:text-xl cursor-pointer sm:w-[150px] sm:h-[150px] w-[50px] h-[50px] flex items-center justify-center border-solid border-2 border-red-400  bg-red-400/80 hover:bg-red-400 rounded-full"
       >
-        Define
+        Empatizar
       </LinkScroll>
       <LinkScroll
-        to="Empathize"
+        to="Definir"
         smooth={true}
         spy={true}
         className="font-bold text-[10px] sm:text-xl cursor-pointer sm:w-[150px] sm:h-[150px] w-[50px] h-[50px] flex items-center justify-center border-solid border-2 border-orange-400  bg-orange-400/80 hover:bg-orange-400 rounded-full"
       >
-        Empathize
+        Definir
       </LinkScroll>
       <LinkScroll
-        to="Ideate"
+        to="Idear"
         smooth={true}
         spy={true}
         className="font-bold text-[10px] sm:text-xl cursor-pointer sm:w-[150px] sm:h-[150px] w-[50px] h-[50px] flex items-center justify-center border-solid border-2 border-yellow-400  bg-yellow-400/80 hover:bg-yellow-400 rounded-full"
       >
-        Ideate
+        Idear
       </LinkScroll>
       <LinkScroll
-        to="Design"
+        to="Diseñar"
         smooth={true}
         spy={true}
         className="font-bold text-[10px] sm:text-xl cursor-pointer sm:w-[150px] sm:h-[150px] w-[50px] h-[50px] flex items-center justify-center border-solid border-2 border-lime-400  bg-lime-400/80 hover:bg-lime-400 rounded-full"
       >
-        Design
+        Diseñar
       </LinkScroll>
       <LinkScroll
-        to="Prototype"
+        to="Prototipar"
         smooth={true}
         spy={true}
         className="font-bold text-[10px] sm:text-xl cursor-pointer sm:w-[150px] sm:h-[150px] w-[50px] h-[50px] flex items-center justify-center border-solid border-2 border-emerald-400  bg-emerald-400/80 hover:bg-emerald-400 rounded-full"
       >
-        Prototype
+        Prototipar
       </LinkScroll>
     </div>
   );
